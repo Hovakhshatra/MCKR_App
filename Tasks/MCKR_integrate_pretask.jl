16244ca264171a58a5ed3c2e07cd78e0f4e69f45
AmirHosein Sadeghimanesh <71024638+Hovakhshatra@users.noreply.github.com>
2020-10-07 22:54:13 +0200

    Update MCKR_integrate_pretask.jl
    Using dictionary structure.

--- Tasks/MCKR_integrate_pretask.jl
+++ Tasks/MCKR_integrate_pretask.jl
@@ -2,7 +2,7 @@
 # The MCKR task is going to compute a single Monte Carlo integration for the given example, parameter box and sample size.
 
 function Do_subtask1(Input_lines)
-    # Reading the inputs from the Input_lines and writes a temperory input julia file for the current tasks.
+    # Reading the inputs from the Input_lines and writes a temporary input julia file for the current tasks.
     Temp_input_file=open(joinpath(pwd(),"Temp","MCKR_integrate_input.jl"),"w")
     for i=1:length(Input_lines) # Detecting the MCKR file.
         line=Input_lines[i]
@@ -66,58 +66,18 @@ function Do_subtask1(Input_lines)
 end
 
 function Do_subtask2()
-    # Making a temperory julia file for non-parallel MC computation.
+    # Making a temporary julia file for non-parallel MC computation.
     include(joinpath(pwd(),"Temp","MCKR_integrate_input.jl"))
     Temp_nonparallel_file=open(joinpath(pwd(),"Temp","MCKR_integrate.jl"),"w")
+    Method_dict=Dict(["Simple",0]=>"sumo",["Simple",1]=>"sumo_with_S",["Antithetic",0]=>"sumo_antithetic",["Antithetic",1]=>"sumo_antithetic_with_S")
+    Time_dict=Dict(1=>["    st=time_ns()\n","    st=time_ns()-st\n",",st/10^9"],0=>["","",",0"])
+    e_hat_dict=Dict(1=>["    standard_error=ans_nonparallel[2]/NN\n    standard_error=standard_error/(NN-1)\n    standard_error=sqrt(standard_error)\n","ans_nonparallel[1]",",standard_error"],0=>["","ans_nonparallel",",0"])
     write(Temp_nonparallel_file,"include(joinpath(pwd(),\"Temp\",\"MCKR_integrate_input.jl\"))
 include(joinpath(pwd(),\"Example_MCKR_bank\",MCKR_file_name))
-function MC_integration(B,NN)")
-    if integration_time_request==1
-    write(Temp_nonparallel_file,"    st=time_ns()")
-        if e_hat_request==1
-            if MC_method=="Simple"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_with_S(B,NN)")
-            elseif MC_method=="Antithetic"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_antithetic_with_S(B,NN)")
-            end
-            write(Temp_nonparallel_file,"\n    standard_error=ans_nonparallel[2]/NN
-    standard_error=standard_error/(NN-1)
-    standard_error=sqrt(standard_error)
-    st=time_ns()-st
-    return(ans_nonparallel[1],standard_error,st/10^9)
+function MC_integration(B,NN)
+$(Time_dict[integration_time_request][1])    ans_nonparallel=$(Method_dict[[MC_method,e_hat_request]])(B,NN)
+$(e_hat_dict[e_hat_request][1])$(Time_dict[integration_time_request][2])    return($(e_hat_dict[e_hat_request][2])$(e_hat_dict[e_hat_request][3])$(Time_dict[integration_time_request][3]))
 end")
-        else
-            if MC_method=="Simple"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo(B,NN)")
-            elseif MC_method=="Antithetic"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_antithetic(B,NN)")
-            end
-            write(Temp_nonparallel_file,"\n    st=time_ns()-st
-    return(ans_nonparallel[1],st/10^9)
-end")
-        end
-    else
-        if e_hat_request==1
-            if MC_method=="Simple"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_with_S(B,NN)")
-            elseif MC_method=="Antithetic"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_antithetic_with_S(B,NN)")
-            end
-            write(Temp_nonparallel_file,"\n    standard_error=ans_nonparallel[2]/NN
-    standard_error=standard_error/(NN-1)
-    standard_error=sqrt(standard_error)
-    return(ans_nonparallel[1],standard_error)
-end")
-        else
-            if MC_method=="Simple"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo(B,NN)")
-            elseif MC_method=="Antithetic"
-                write(Temp_nonparallel_file,"\n    ans_nonparallel=sumo_antithetic(B,NN)")
-            end
-            write(Temp_nonparallel_file,"\n    return(ans_nonparallel[1])
-end")
-        end
-    end
     close(Temp_nonparallel_file)
 end
 
