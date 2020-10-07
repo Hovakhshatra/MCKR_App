c92e7af3343b366e43a62c8e03e8d8c598925c6d
AmirHosein Sadeghimanesh <71024638+Hovakhshatra@users.noreply.github.com>
2020-10-07 23:16:15 +0200

    Update MCKR_bisect_search_paral_tracker.jl
    Using dictionary structure.

--- Tasks/MCKR_bisect_search_paral_tracker.jl
+++ Tasks/MCKR_bisect_search_paral_tracker.jl
@@ -19,7 +19,7 @@ function Do_task()
     close(Tracker_file)
     # Adding extra workers.
     addprocs(Workers_number)
-    # Loading the temperory input file for the extra workers as well.
+    # Loading the temporary input file for the extra workers as well.
     @everywhere include(joinpath(pwd(),"Temp","MCKR_bisect_search_paral_input.jl"))
     # Loading the MCKR file for all workers.
     @everywhere include(joinpath(pwd(),"Example_MCKR_bank",MCKR_file_name))
@@ -27,18 +27,13 @@ function Do_task()
     out1=MC_search(Box,Sample_size,Stop_criterion_bisect_step)
     if Searching_time_request==1
         push!(Task_output,"\n$(out1[1]).\nIt took $(out1[3]) seconds and computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[4])\nThe last sub-box = $(out1[5])\nwith I-hat = $(out1[6])\n")
-        if Last_box_info_request==1
-            out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
-            out2=MC_integration(out1[5],Sample_size)
-            push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
-        end
     else
-        push!(Task_output,"\n$(out1[1]).\nIt computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[4])\nThe last sub-box = $(out1[5])\nwith I-hat = $(out1[6])\n")
-        if Last_box_info_request==1
-            out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
-            out2=MC_integration(out1[5],Sample_size)
-            push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
-        end
+        push!(Task_output,"\n$(out1[1]).\nIt computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[3])\nThe last sub-box = $(out1[4])\nwith I-hat = $(out1[5])\n")
+    end
+    if Last_box_info_request==1
+        out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
+        out2=MC_integration(out1[5],Sample_size)
+        push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
     end
     push!(Task_output,"\n(Parallelization using $Workers_number workers is used)\n\n\u066d End of computations.")
     for i=2:1+Workers_number
