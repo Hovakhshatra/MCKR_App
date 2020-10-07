6540b90e2f52badb5bf2f1c81ab0a666b58ffb8b
AmirHosein Sadeghimanesh <71024638+Hovakhshatra@users.noreply.github.com>
2020-10-07 22:58:26 +0200

    Update MCKR_integrate_paral_pretask.jl
    Using dictionary structure.

--- Tasks/MCKR_integrate_paral_pretask.jl
+++ Tasks/MCKR_integrate_paral_pretask.jl
@@ -2,7 +2,7 @@
 # The MCKR task is going to compute a single Monte Carlo integration for the given example, parameter box and sample size.
 
 function Do_subtask1(Input_lines)
-    # Reading the inputs from the Input_lines and writes a temperory input julia file for the current tasks.
+    # Reading the inputs from the Input_lines and writes a temporary input julia file for the current tasks.
     Temp_input_file=open(joinpath(pwd(),"Temp","MCKR_integrate_paral_input.jl"),"w")
     for i=1:length(Input_lines) # Detecting the MCKR file.
         line=Input_lines[i]
@@ -80,116 +80,36 @@ function Do_subtask1(Input_lines)
 end
 
 function Do_subtask2()
-    # Making a temperory julia file for parallel case.
+    # Making a temporary julia file for parallel case.
     include(joinpath(pwd(),"Temp","MCKR_integrate_paral_input.jl"))
     Temp_parallel_file=open(joinpath(pwd(),"Temp","MCKR_integrate_paral.jl"),"w")
+
+    Method_dict=Dict(["Simple",0]=>"sumo",["Simple",1]=>"sumo_with_S",["Antithetic",0]=>"sumo_antithetic",["Antithetic",1]=>"sumo_antithetic_with_S")
+    Time_dict=Dict(1=>["    st=time_ns()\n","    st=time_ns()-st\n",",st/10^9"],0=>["","",",0"])
+    e_hat_dict=Dict(1=>["[1]","    standard_error=ans2_2/(Workers_number*floor(NN/Workers_number))\n    standard_error=standard_error/((Workers_number*floor(NN/Workers_number))-1)\n    standard_error=sqrt(standard_error)\n",",standard_error"],0=>["","",",0"])
     write(Temp_parallel_file,"include(joinpath(pwd(),\"Temp\",\"MCKR_integrate_paral_input.jl\"))
 include(joinpath(pwd(),\"Example_MCKR_bank\",MCKR_file_name))
-function MC_integration(B,NN)\n")
-    if integration_time_request==1
-        write(Temp_parallel_file,"    st=time_ns()\n")
-        if e_hat_request==1
-            if MC_method=="Simple"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_with_S,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            elseif MC_method=="Antithetic"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_antithetic_with_S,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            end
-            for i=1:Workers_number
-                write(Temp_parallel_file,"    ans2w$(i)=fetch(w$(i)ans1)\n")
-            end
-            write(Temp_parallel_file,"    ans2_1=(")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)[1]+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number[1])/Workers_number\n")
-            write(Temp_parallel_file,"    ans2_2=")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)[2]+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number[2]
-    standard_error=ans2_2/(Workers_number*floor(NN/Workers_number))
-    standard_error=standard_error/((Workers_number*floor(NN/Workers_number))-1)
-    standard_error=sqrt(standard_error)
-    st=time_ns()-st
-    return(ans2_1,standard_error,st/10^9)
-end")
-        else
-            if MC_method=="Simple"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            elseif MC_method=="Antithetic"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_antithetic,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            end
-            for i=1:Workers_number
-                write(Temp_parallel_file,"    ans2w$(i)=fetch(w$(i)ans1)\n")
-            end
-            write(Temp_parallel_file,"    ans2_1=(")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number)/Workers_number\n
-    st=time_ns()-st
-    return(ans2_1,st/10^9)
-end")
-        end
-    else
-        if e_hat_request==1
-            if MC_method=="Simple"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_with_S,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            elseif MC_method=="Antithetic"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_antithetic_with_S,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            end
-            for i=1:Workers_number
-                write(Temp_parallel_file,"    ans2w$(i)=fetch(w$(i)ans1)\n")
-            end
-            write(Temp_parallel_file,"    ans2_1=(")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)[1]+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number[1])/Workers_number\n")
-            write(Temp_parallel_file,"    ans2_2=")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)[2]+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number[2]
-    standard_error=ans2_2/(Workers_number*floor(NN/Workers_number))
-    standard_error=standard_error/((Workers_number*floor(NN/Workers_number))-1)
-    standard_error=sqrt(standard_error)
-    return(ans2_1,standard_error)
-end")
-        else
-            if MC_method=="Simple"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            elseif MC_method=="Antithetic"
-                for i=1:Workers_number
-                    write(Temp_parallel_file,"    w$(i)ans1=remotecall(sumo_antithetic,$(1+i),B,floor(NN/Workers_number))\n")
-                end
-            end
-            for i=1:Workers_number
-                write(Temp_parallel_file,"    ans2w$(i)=fetch(w$(i)ans1)\n")
-            end
-            write(Temp_parallel_file,"    ans2_1=(")
-            for i=1:Workers_number-1
-                write(Temp_parallel_file,"ans2w$(i)+")
-            end
-            write(Temp_parallel_file,"ans2w$Workers_number)/Workers_number\n
-    return(ans2_1)
-end")
-        end
+function MC_integration(B,NN)\n$(Time_dict[integration_time_request][1])")
+    for i=1:Workers_number
+        write(Temp_parallel_file,"    w$(i)ans1=remotecall($(Method_dict[[MC_method,e_hat_request]]),$(1+i),B,floor(NN/Workers_number))\n")
     end
+    for i=1:Workers_number
+        write(Temp_parallel_file,"    ans2w$(i)=fetch(w$(i)ans1)\n")
+    end
+    write(Temp_parallel_file,"    ans2_1=(")
+    for i=1:Workers_number-1
+        write(Temp_parallel_file,"ans2w$(i)$(e_hat_dict[e_hat_request][1])+")
+    end
+    write(Temp_parallel_file,"ans2w$Workers_number$(e_hat_dict[e_hat_request][1]))/Workers_number\n")
+    if e_hat_request==1
+        write(Temp_parallel_file,"    ans2_2=")
+        for i=1:Workers_number-1
+            write(Temp_parallel_file,"ans2w$(i)[2]+")
+        end
+        write(Temp_parallel_file,"ans2w$Workers_number[2]\n")
+    end
+    write(Temp_parallel_file,"$(e_hat_dict[e_hat_request][2])$(Time_dict[integration_time_request][2])    return(ans2_1$(e_hat_dict[e_hat_request][3])$(Time_dict[integration_time_request][3]))
+end")
     close(Temp_parallel_file)
 end
 
