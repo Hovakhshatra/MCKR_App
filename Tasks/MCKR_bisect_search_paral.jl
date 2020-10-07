dbc00766e072124de7ab8fc7129cfa39092f148d
AmirHosein Sadeghimanesh <71024638+Hovakhshatra@users.noreply.github.com>
2020-10-07 23:13:00 +0200

    Update MCKR_bisect_search_paral.jl
    Using dictionary structure.

--- Tasks/MCKR_bisect_search_paral.jl
+++ Tasks/MCKR_bisect_search_paral.jl
@@ -15,7 +15,7 @@ function Do_task()
     push!(Task_output,"\nConsider the bisect search using the $MC_method Monte Carlo integration with N=$Sample_size for the $(MCKR_file_name[1:end-3]) example with the following initial box.\n$Box\nand with the stop criteria as follows;\nsub-box size limit = $Stop_criterion_subbox_size\nlimit on the number of bisecting steps = $Stop_criterion_bisect_step\n")
     # Adding extra workers.
     addprocs(Workers_number)
-    # Loading the temperory input file for the extra workers as well.
+    # Loading the temporary input file for the extra workers as well.
     @everywhere include(joinpath(pwd(),"Temp","MCKR_bisect_search_paral_input.jl"))
     # Loading the MCKR file for all workers.
     @everywhere include(joinpath(pwd(),"Example_MCKR_bank",MCKR_file_name))
@@ -23,18 +23,13 @@ function Do_task()
     out1=MC_search(Box,Sample_size,Stop_criterion_bisect_step)
     if Searching_time_request==1
         push!(Task_output,"\n$(out1[1]).\nIt took $(out1[3]) seconds and computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[4])\nThe last sub-box = $(out1[5])\nwith I-hat = $(out1[6])\n")
-        if Last_box_info_request==1
-            out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
-            out2=MC_integration(out1[5],Sample_size)
-            push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
-        end
     else
-        push!(Task_output,"\n$(out1[1]).\nIt computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[4])\nThe last sub-box = $(out1[5])\nwith I-hat = $(out1[6])\n")
-        if Last_box_info_request==1
-            out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
-            out2=MC_integration(out1[5],Sample_size)
-            push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
-        end
+        push!(Task_output,"\n$(out1[1]).\nIt computed $(out1[2]) integrals.\nNumber of bisecting step = $(out1[3])\nThe last sub-box = $(out1[4])\nwith I-hat = $(out1[5])\n")
+    end
+    if Last_box_info_request==1
+        out2=MC_integration(out1[5],10*Workers_number) # just to warm-up Julia for this function.
+        out2=MC_integration(out1[5],Sample_size)
+        push!(Task_output,"\nComputing the MCKR on the last sub-box again.\nI-hat = $(out2[1]), e-hat = $(out2[2]))\n")
     end
     push!(Task_output,"\n(Parallelization using $Workers_number workers is used)\n\n\u066d End of computations.")
     for i=2:1+Workers_number
