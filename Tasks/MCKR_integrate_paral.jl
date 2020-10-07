33675c77f452bf20e0aed2215b2b93e0f8862a38
AmirHosein Sadeghimanesh <71024638+Hovakhshatra@users.noreply.github.com>
2020-10-07 22:55:57 +0200

    Update MCKR_integrate_paral.jl
    Using dictionary structure.

--- Tasks/MCKR_integrate_paral.jl
+++ Tasks/MCKR_integrate_paral.jl
@@ -13,25 +13,17 @@ function Do_task()
     push!(Task_output,"\nConsider the $MC_method Monte Carlo integration for the $(MCKR_file_name[1:end-3]) example on the box\n$Box\nand N=$Sample_size.\n")
     # Adding extra workers.
     addprocs(Workers_number)
-    # Loading the temperory input file for the extra workers as well.
+    # Loading the temporary input file for the extra workers as well.
     @everywhere include(joinpath(pwd(),"Temp","MCKR_integrate_paral_input.jl"))
     # Loading the MCKR file for all workers.
     @everywhere include(joinpath(pwd(),"Example_MCKR_bank",MCKR_file_name))
     out1=MC_integration(Box,10*Workers_number) # Just to warm up Julia about this function.
     out1=MC_integration(Box,Sample_size)
-    if integration_time_request==1
-        if e_hat_request==1
-            push!(Task_output,"\nI-hat = $(out1[1]), e-hat = $(out1[2]), integration time = $(out1[3]) seconds\n(Parallelization using $Workers_number workers is used)\n")
-        else
-            push!(Task_output,"\nI-hat = $(out1[1]), integration time = $(out1[3]) seconds\n(Parallelization using $Workers_number workers is used)\n")
-        end
-    else
-        if e_hat_request==1
-            push!(Task_output,"\nI-hat = $(out1[1]), e-hat = $(out1[2])\n(Parallelization using $Workers_number workers is used)\n")
-        else
-            push!(Task_output,"\nI-hat = $(out1[1])\n(Parallelization using $Workers_number workers is used)\n")
-        end
-    end
+    Output_dict=Dict([1,1]=>"\nI-hat = $(out1[1]), e-hat = $(out1[2]), integration time = $(out1[3]) seconds\n(Parallelization using $Workers_number workers is used)\n",
+                     [1,0]=>"\nI-hat = $(out1[1]), integration time = $(out1[3]) seconds\n(Parallelization using $Workers_number workers is used)\n",
+                     [0,1]=>"\nI-hat = $(out1[1]), e-hat = $(out1[2])\n(Parallelization using $Workers_number workers is used)\n",
+                     [0,0]=>"\nI-hat = $(out1[1])\n(Parallelization using $Workers_number workers is used)\n")
+    push!(Task_output,Output_dict[[integration_time_request,e_hat_request]])
     for i=2:1+Workers_number
         rmprocs(i,waitfor=0)
     end
